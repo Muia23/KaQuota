91383af5aa2fa4d24fbf1cb5ade1fb334b0822c6
Muia23 <wilfred.muema@gmail.com>
2020-06-15 06:10:20 +0300

    I import and added the pipe to the declarations array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotaComponent } from './quota/quota.component';
+import { LandingComponent } from './landing/landing.component';
+import { HeaderComponent } from './header/header.component';
+import { FamousComponent } from './famous/famous.component';
+import { QuotaFormComponent } from './quota-form/quota-form.component';
+import { QuotaDetailsComponent } from './quota-details/quota-details.component';
+import { DateAgoPipe } from './pipes/date-ago.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotaComponent
+    QuotaComponent,
+    LandingComponent,
+    HeaderComponent,
+    FamousComponent,
+    QuotaFormComponent,
+    QuotaDetailsComponent,
+    DateAgoPipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
