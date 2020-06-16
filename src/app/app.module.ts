416dec2baa431c866faf40a4896c9d83528597bd
Muia23 <wilfred.muema@gmail.com>
2020-06-16 03:59:14 +0300

    add boostrap link to styles array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotaComponent } from './quota/quota.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { LandingComponent } from './landing/landing.component';
 import { HeaderComponent } from './header/header.component';
 import { FamousComponent } from './famous/famous.component';
+
 import { QuotaFormComponent } from './quota-form/quota-form.component';
 import { QuotaDetailsComponent } from './quota-details/quota-details.component';
 import { DateAgoPipe } from './pipes/date-ago.pipe';
@@ -25,7 +29,8 @@ import { DateAgoPipe } from './pipes/date-ago.pipe';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
