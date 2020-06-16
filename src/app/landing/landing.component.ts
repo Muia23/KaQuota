624fe224321be8c1e7ed40e443dde3f95ae69cde
Muia23 <wilfred.muema@gmail.com>
2020-06-16 03:49:10 +0300

    remove unecessary goal import

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Quota } from '../quota'
 
 @Component({
   selector: 'app-landing',
@@ -7,16 +6,7 @@ import { Quota } from '../quota'
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
-
-  quotas: Quota[]= [
-    new Quota(1, 'Username','The first Quote'),
-    new Quota(2, 'Username', 'The second Quote')
-  ]
-  addNewQuota(quota){
-    let quotaLength = this.quotas.length;
-    quota.id = quotaLength +1;
-    this.quotas.push(quota)
-  }
+  
   constructor() { }
 
   ngOnInit(): void {
