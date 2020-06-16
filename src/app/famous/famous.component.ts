4c7aa3c254e8a969fda09c34da9d24f128282d90
Muia23 <wilfred.muema@gmail.com>
2020-06-16 03:44:38 +0300

    add upvote and downvote function for trending quotes

--- src/app/famous/famous.component.ts
+++ src/app/famous/famous.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-famous',
   templateUrl: './famous.component.html',
@@ -12,4 +13,43 @@ export class FamousComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  likeClick() {
+  let a = document.getElementById("number1");
+  let b = document.getElementById("number2");
+    if (a.innerHTML === "1,456") {
+      a.innerHTML = "1,457";
+    } else if (a.innerHTML ==="1,457") {
+      a.innerHTML = "1,456";
+    } else {
+      a.innerHTML = "1,456";
+    }
+
+    if (b.innerHTML === "890") {
+      b.innerHTML = "891";
+    } else if (b.innerHTML ==="891") {
+      b.innerHTML = "890";
+    } else {
+      b.innerHTML = "890";
+    }
+}
+
+dislikeClick() {
+  let c = document.getElementById("number1");
+  let d = document.getElementById("number2");
+    if (c.innerHTML === "1,456") {
+      c.innerHTML = "1,455";
+    } else if (c.innerHTML ==="1,455") {
+      c.innerHTML = "1,456";
+    } else {
+      c.innerHTML = "1,456";
+    }
+
+    if (d.innerHTML === "890") {
+      d.innerHTML = "889";
+    } else if (d.innerHTML ==="889") {
+      d.innerHTML = "890";
+    } else {
+      d.innerHTML = "890";
+    }
+}
 }
